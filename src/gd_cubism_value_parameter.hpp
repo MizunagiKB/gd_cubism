bf4d4164509e0ac31b3ee3aaae7394ee94546583
MizunagiKB <mizukb@LIVE.JP>
2024-08-15 07:58:48 +0900

    Added a property called hold to hold the parameter.

--- src/gd_cubism_value_parameter.hpp
+++ src/gd_cubism_value_parameter.hpp
@@ -43,6 +43,7 @@ private:
     float minimum_value;
     float maximum_value;
     float default_value;
+    bool hold;
 
 protected:
     static void _bind_methods() {
@@ -53,6 +54,10 @@ protected:
         ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "maximum_value"), String(), "get_maximum_value");
         ClassDB::bind_method(D_METHOD("get_default_value"), &GDCubismParameter::get_default_value);
         ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "default_value"), String(), "get_default_value");
+
+        ClassDB::bind_method(D_METHOD("set_hold"), &GDCubismParameter::set_hold);
+        ClassDB::bind_method(D_METHOD("get_hold"), &GDCubismParameter::get_hold);
+        ADD_PROPERTY(PropertyInfo(Variant::BOOL, "hold"), "set_hold", "get_hold");
 
         ClassDB::bind_method(D_METHOD("reset"), &GDCubismParameter::reset);
 
@@ -73,16 +78,20 @@ private:
         this->minimum_value = Core::csmGetParameterMinimumValues(csm_model)[index];
         this->maximum_value = Core::csmGetParameterMaximumValues(csm_model)[index];
         this->default_value = Core::csmGetParameterDefaultValues(csm_model)[index];
+        this->hold = false;
     }
 
 public:
     GDCubismParameter()
         : GDCubismValueAbs(GDCubismValueAbs::ValueType::PARAMETER) {}
 
-    void reset() { this->set_value(this->default_value); }
+    void reset() { this->set_value(this->default_value); this->hold = false; }
     float get_minimum_value() const { return this->minimum_value; }
     float get_maximum_value() const { return this->maximum_value; }
     float get_default_value() const { return this->default_value; }
+
+    void set_hold(const bool value) { this->hold = value; }
+    bool get_hold() const { return this->hold; }
 };
 
 VARIANT_ENUM_CAST(GDCubismParameter::Type);
